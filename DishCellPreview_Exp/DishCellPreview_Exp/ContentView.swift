52846a0f73dd36c013a95945754c36a390821fa6
Shak49 <shakfeizi@gmali.com>
2022-08-02 15:06:56 -0500

    list implemented

--- DishCellPreview_Exp/DishCellPreview_Exp/ContentView.swift
+++ DishCellPreview_Exp/DishCellPreview_Exp/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let dishes = Dish.all()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(dishes, id: \.self) { dish in
+                    DishCell(dish: dish)
+                }
+            }
+            .listStyle(.plain)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .colorScheme(.dark)
     }
 }

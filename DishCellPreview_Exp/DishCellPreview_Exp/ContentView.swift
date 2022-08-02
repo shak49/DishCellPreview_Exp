bcc2bf3919fc3a94f5ba469d2edfaa0a4a734618
Shak49 <shakfeizi@gmali.com>
2022-08-02 13:58:08 -0500

    project files Added

---
+++ DishCellPreview_Exp/DishCellPreview_Exp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DishCellPreview_Exp
+//
+//  Created by Shak Feizi on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DishCellPreview_Exp/DishCellPreview_Exp/DishCellPreview_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishCellPreview_ExpApp.swift
+//  DishCellPreview_Exp
+//
+//  Created by Shak Feizi on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DishCellPreview_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
